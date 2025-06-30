3122d493d26e15da89a061c6fef6948433f9ccd9
Leandro Aguiar <aguiarleandroferreira@gmail.com>
2025-06-30 15:47:51 -0300

    Atualização do novo exercício

---
+++ mundo1/ex007/README.md
@@ -0,0 +1,7 @@
+# Exercício 7 
+
+Desenvolva um programa que leia duas notas de um aluno e mostre a sua média.
+
+# Exercise 7 
+
+Develop a program that reads two grades of a student and calculates their average.
---
+++ mundo1/ex007/ex007.py
@@ -0,0 +1,16 @@
+#boas-vindas ao programa.
+
+print ("Olá, Professor(a)! Vamos calcular a média das duas provas! ")
+
+#inserir as notas das provas 
+
+p1 = float (input ("Insira a nota da primeira prova: "))
+p2 = float (input ("Insira a nota da segunda prova: "))
+
+#realizar o calculo das notas e fazer a média
+
+m = (p1 + p2) / 2
+
+#informar na tela o resultado do cálculo
+
+print ("O seu aluno ficará com a média de: {}".format(m))
