439da5c09475618cb579b0f315ecaa14c51614cc
Leandro Aguiar <aguiarleandroferreira@gmail.com>
2025-07-01 10:32:54 -0300

    Novo exercício

---
+++ mundo1/ex009/ex009.py
@@ -0,0 +1,31 @@
+#boas-vinas e pedido de entrada do valor
+
+print ("Bem-vindo(a) a calculadora de tabuada.")
+v = float (input ("Por favor, insira um valor para eu multiplicar pra você! "))
+
+#calculando o valor até o 10
+
+r = v * 1 
+r1 = v * 2 
+r2 = v * 3 
+r3 = v * 4 
+r4 = v * 5 
+r5 = v * 6 
+r6 = v * 7 
+r7 = v * 8 
+r8 = v * 9
+r9 = v * 10 
+
+#resultados na tela do usuário
+
+print ("Seu valor inserido é: {}!".format(v))
+print ("{} * 1 = {}".format(v,r ))
+print ("{} * 2 = {}".format(v,r1 ))
+print ("{} * 3 = {}".format(v,r2 ))
+print ("{} * 4 = {}".format(v,r3 ))
+print ("{} * 5 = {}".format(v,r4 ))
+print ("{} * 6 = {}".format(v,r5 ))
+print ("{} * 7 = {}".format(v,r6 ))
+print ("{} * 8 = {}".format(v,r7 ))
+print ("{} * 9 = {}".format(v,r8 ))
+print ("{} * 10 = {}".format(v,r9 ))
