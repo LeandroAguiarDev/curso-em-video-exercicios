2bb49c51a7abc95457c46b42c4460d958c950dc7
Leandro Aguiar <aguiarleandroferreira@gmail.com>
2025-07-03 21:36:44 -0300

    Correção dos exercícios 7, 8 e 9

--- mundo1/ex008/ex008.py
+++ mundo1/ex008/ex008.py
@@ -3,13 +3,6 @@
 print ("Olá, bem-vindo(a)! Sou um conversor de Metros para centímetros e milímetros! ")
 m = float (input ("Insira um valor em metros para a conversão: "))
 
-#realizar a conversão
-
-cm = m * 100
-mm = m * 1000
-
 #resolução para aprensentar na tela
 
-print ("O seu valor é {} metros, em centímetros é: {} CM, em milímetros é: {} MM! ".format(m, cm, mm))
-
-
+print ("O seu valor é {} metros, em centímetros é: {:.2f} CM, em milímetros é: {:.2f} MM! ".format(m, (m * 100), (m * 1000)))
--- mundo1/ex009/ex009.py
+++ mundo1/ex009/ex009.py
@@ -1,31 +1,20 @@
 #boas-vinas e pedido de entrada do valor
 
 print ("Bem-vindo(a) a calculadora de tabuada.")
-v = float (input ("Por favor, insira um valor para eu multiplicar pra você! "))
-
-#calculando o valor até o 10
-
-r = v * 1 
-r1 = v * 2 
-r2 = v * 3 
-r3 = v * 4 
-r4 = v * 5 
-r5 = v * 6 
-r6 = v * 7 
-r7 = v * 8 
-r8 = v * 9
-r9 = v * 10 
+v = float (input ("Por favor, insira um valor para eu multiplicar pra você: "))
 
 #resultados na tela do usuário
 
+print ('=' * 18)
 print ("Seu valor inserido é: {}!".format(v))
-print ("{} * 1 = {}".format(v,r ))
-print ("{} * 2 = {}".format(v,r1 ))
-print ("{} * 3 = {}".format(v,r2 ))
-print ("{} * 4 = {}".format(v,r3 ))
-print ("{} * 5 = {}".format(v,r4 ))
-print ("{} * 6 = {}".format(v,r5 ))
-print ("{} * 7 = {}".format(v,r6 ))
-print ("{} * 8 = {}".format(v,r7 ))
-print ("{} * 9 = {}".format(v,r8 ))
-print ("{} * 10 = {}".format(v,r9 ))
+print ("{} x {:2} = {}".format(v,1, v * 1 ))
+print ("{} x {:2} = {}".format(v,2, v * 2 ))
+print ("{} x {:2} = {}".format(v,3, v * 3 ))
+print ("{} x {:2} = {}".format(v,4, v * 4 ))
+print ("{} x {:2} = {}".format(v,5, v * 5 ))
+print ("{} x {:2} = {}".format(v,6, v * 6 ))
+print ("{} x {:2} = {}".format(v,7, v * 7 ))
+print ("{} x {:2} = {}".format(v,8, v * 8 ))
+print ("{} x {:2} = {}".format(v,9, v * 9 ))
+print ("{} x {} = {}".format(v,10, v * 10 ))
+print ('=' * 18)
