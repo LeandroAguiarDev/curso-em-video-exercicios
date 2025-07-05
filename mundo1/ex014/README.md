7206c08a5b88acb4d68e16250c10bd7acedc0ac5
Leandro Aguiar <aguiarleandroferreira@gmail.com>
2025-07-05 11:58:27 -0300

    Adicionando ex014.

---
+++ mundo1/ex014/README.md
@@ -0,0 +1,7 @@
+# Exercício 14
+
+Escreva um programa que converta uma temparatura digitada em °C e converta para °F.
+
+# Exercise 14
+
+Write a program that convert a temperature from °C to °F.
---
+++ mundo1/ex014/ex014.py
@@ -0,0 +1,17 @@
+# Apresentação do programa e interação para recolher informações
+
+print ("Bem-vindo(a), sou um conversor de ºC para Fº ")
+c = float (input ("Insira o valor em ºC: "))
+
+# Cálcular a conversão
+
+# cc = (c * 1.18) + 32
+
+# Mostrar o resultado na tela 
+
+print ("{}ºC é o mesmo que {}ºF. ".format(c, ((c * 1.8) + 32)))
+
+# Legendas:
+# c = Celcius
+# cc = cálcular conversão
+
